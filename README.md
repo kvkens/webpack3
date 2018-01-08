a785e7cb8cd80661fc34b2185a2ecbed8bf74538
kvkens <kvkens@qq.com>
2018-01-08 21:34:34 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# react 基本脚手架
+# react 脚手架
 
-## 集成说明
+## 安装
 
-- `react`16.0.0
-- `webpack`3.8.1
-- `react-router-dom`4.2.2
+- 下载本仓库执行 `npm install`
+- 开启服务 `npm run dev`
+- 构建资源 `npm run build`
+
+## 特性
+
+- react
+- redux
+- react-router
+- webpack
+- babel
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,67 +6,79 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
-    vendor: ["react", "react-dom"],
-    app: "./index.js"
+    vendor: [
+      "react", "react-dom", "react-router-dom"
+    ],
+    app: "./src/index"
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist")
+    filename: "[name].[hash:8].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: ""
   },
   externals: {
-    //"react": "React",
-    //"react-dom": "ReactDOM"
+    //"react": "React", "react-dom": "ReactDOM"
   },
-  resolve : {
+  resolve: {
+    extensions: [
+      ".jsx", ".js", ".json"
+    ],
     alias: {
       containers: path.resolve(__dirname, "src/containers/"),
       components: path.resolve(__dirname, "src/components/"),
-      assets: path.resolve(__dirname, "src/assets/"),
-      routes: path.resolve(__dirname, "src/routes/")
+      assets: path.resolve(__dirname, "src/assets/")
     }
   },
   module: {
-    rules : [{
-      test : /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use : [
-        "babel-loader"
-      ]
-    },{
-      test : /\.css$/,
-      use: ExtractTextPlugin.extract({
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve("src"),
+        use: ["babel-loader"]
+      }, {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader', 'postcss-loader', 'less-loader'
+          ],
+          fallback: 'style-loader'
+        })
+      }, {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ["css-loader","postcss-loader"]
-      })
-    },{
-      test : /\.(png|svg|jpg|gif)$/,
-      use : [{
-        loader: "file-loader",
-        options: {
-          name: "images/[name].[ext]"
-        }
-      }]
-    },{
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use : [
-        "file-loader"
-      ]
-    }]
+          use: ["css-loader", "postcss-loader"]
+        })
+      }, {
+        test: /\.(png|jpg|jpeg|gif)(\?.+)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "images/[name].[hash:8].[ext]",
+              limit: 8192
+            }
+          }
+        ]
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[hash:8].[ext]"
+            }
+          }
+        ]
+      }
+    ]
   },
-  plugins : [
-    new webpack.optimize.UglifyJsPlugin({
-      compress : false
-    }),
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({compress: false}),
     new ExtractTextPlugin("[name].css"),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor"
-    }),
+    new webpack.optimize.CommonsChunkPlugin({name: "vendor"}),
     new CleanWebpackPlugin(["dist"]),
-    new HtmlWebpackPlugin({
-      template : "./index.html",
-      inject : "body",
-      favicon : "./src/assets/favicon.ico",
-      hash : true
-    })
+    new HtmlWebpackPlugin({filename: "index.html", template: "./src/index.html", inject: "body", favicon: "./src/assets/favicon.ico", hash: true})
   ]
 };
